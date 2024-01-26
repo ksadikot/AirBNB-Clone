d5f900591ca23dfdff974333cfcb8997eaf39d1e
Kaid-Johar Sadikot <ksadikot@uoguelph.ca>
2024-01-26 17:16:13 -0500

    nav bar

---
+++ src/components/Navbar.js
@@ -0,0 +1,11 @@
+import logo from '../images/airbnb-logo.png'
+
+export default function Navbar() {
+    return (
+        <nav className='nav'>
+            <img src={logo} alt='airbnb-logo' className='nav--logo'/>
+        </nav>
+    )
+
+    
+}
